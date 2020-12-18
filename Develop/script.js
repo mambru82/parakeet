bb2b12069e97c9d9bd1ec1f73717c6be257f86ba
mambru <ecastro82@gmail.com>
2020-12-18 11:20:23 -0600

    basic password function with lowercase

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,32 @@
 // Assignment code here
 //WHEN I click the button to generate a password
-// -- prompt to generate a password 
+var specialCharacters ="\"$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+var numeric = "0123456789";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//random number generator function
+function randomNumber (min, max) {
+  var value = Math.floor(Math.random() * (max-min +1) + min);
+
+  return value;
+};
+
+for (var i=0; i<specialCharacters.length; i++) {
+  console.log (specialCharacters[i], lowercase[i], numeric[i], uppercase[i]);
+}
 
 function generatePassword () {
   console.log ("end of generatePassword function");
+  var passwordLength = prompt("Please enter the password length (8 to 128 characters)");
+  console.log(passwordLength);
+  password = "";
+ for (var i=0; i<passwordLength; i++) {
+   var passchar = lowercase[randomNumber(0,(lowercase.length-1))];
+   password = password + passchar;
+  }
+  console.log(password, password.length);
 }
+
 //THEN I am presented with a series of prompts for password criteria
 //WHEN prompted for password criteria
 //THEN I select which criteria to include in the password
@@ -24,6 +46,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // -- prompt to generate a password when user presses button
   if (window.confirm("If you would like to proceed with password generation, press OK")) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
